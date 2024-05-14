83516ea5e166f6f8b3b1536a6bcb27a96da33d75
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-05-14 22:11:33 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Bugra Aslan on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
